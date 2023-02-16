ccde44cc088d6aa0a25299d4e9d5bda98af3226e
Mark Donnelly <mark@painless-security.com>
2023-02-16 12:53:26 -0500

    Add MetricNameConfigurationTransformer
    This will add an attribute for what to name the metric in OpenCensus to each entry

--- ldapstats/configuration.py
+++ ldapstats/configuration.py
@@ -226,6 +226,51 @@ class ChildObjectConfigurationTransformer(ConfigurationTransformer):
             return configuration
 
 
+class MetricNameConfigurationTransformer(ConfigurationTransformer):
+    @staticmethod
+    def process(configuration):
+        if not isinstance(configuration, dict):
+            raise ValueError(
+                'NameConfigurationTransformer expect a configuration that is a dict but received one that isn\'t.')
+        config = copy.deepcopy(configuration)
+        object_config = config.get('object')
+        if object_config:
+            config['object'] = MetricNameConfigurationTransformer.process_object_config(
+                configuration=object_config
+            )
+        return config
+
+    @staticmethod
+    def process_object_config(configuration, prefix='', default_name='', separator='/'):
+        full_prefix = prefix + separator if prefix else ''
+        if isinstance(configuration, dict):
+            config = {}
+            metric_name = prefix
+            if configuration.get('rdn') or configuration.get('attribute'):
+                metric_name = full_prefix + configuration.get('name', default_name)
+                config['metric_name'] = metric_name
+            for key, value in configuration.items():
+                config[key] = MetricNameConfigurationTransformer.process_object_config(
+                    configuration=value,
+                    prefix=metric_name,
+                    default_name=key,
+                    separator=separator
+                )
+            return config
+        elif isinstance(configuration, list):
+            return [
+                MetricNameConfigurationTransformer.process_object_config(
+                    configuaration=item,
+                    prefix=prefix,
+                    default_name=default_name,
+                    separator=separator
+                )
+                for item in configuration
+            ]
+        else:
+            return configuration
+
+
 def read_yaml_file(file_name):
     with open(file_name, 'r') as file:
         ret_val = yaml.safe_load(file)
