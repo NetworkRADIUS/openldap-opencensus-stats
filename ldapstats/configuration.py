baba1a212fa3e10d6ff579e87818e7910fca6f6e
Mark Donnelly <mark@painless-security.com>
2023-02-16 13:14:25 -0500

    Add MetricDnConfigurationTransformer
    This will add an attribute to each entry for what LDAP DN to use

--- ldapstats/configuration.py
+++ ldapstats/configuration.py
@@ -271,6 +271,57 @@ class MetricNameConfigurationTransformer(ConfigurationTransformer):
             return configuration
 
 
+class MetricDnConfigurationTransformer(ConfigurationTransformer):
+    @staticmethod
+    def process(configuration):
+        if not isinstance(configuration, dict):
+            raise ValueError(
+                'NameConfigurationTransformer expect a configuration that is a dict but received one that isn\'t.')
+        config = copy.deepcopy(configuration)
+        object_config = config.get('object')
+        if object_config:
+            config['object'] = MetricDnConfigurationTransformer.process_object_config(
+                configuration=object_config
+            )
+        return config
+
+    @staticmethod
+    def process_object_config(configuration, suffix_dn='', separator=','):
+        if isinstance(configuration, dict):
+            config = {}
+            if suffix_dn:
+                full_suffix = separator + suffix_dn
+            else:
+                full_suffix = suffix_dn
+            computed_dn = suffix_dn
+            if configuration.get('rdn'):
+                computed_dn = configuration.get('rdn') + full_suffix
+
+            # True if either 'rdn' or 'attribute' is a key
+            if configuration.get('rdn', configuration.get('attribute')):
+                config['computed_dn'] = computed_dn
+            else:
+                computed_dn = suffix_dn
+            for key, value in configuration.items():
+                config[key] = MetricDnConfigurationTransformer.process_object_config(
+                    configuration=value,
+                    suffix_dn=computed_dn,
+                    separator=separator
+                )
+            return config
+        elif isinstance(configuration, list):
+            return [
+                MetricDnConfigurationTransformer.process_object_config(
+                    configuaration=item,
+                    suffix_dn=suffix_dn,
+                    separator=separator
+                )
+                for item in configuration
+            ]
+        else:
+            return configuration
+
+
 def read_yaml_file(file_name):
     with open(file_name, 'r') as file:
         ret_val = yaml.safe_load(file)
