131a87ca57ce9896848bb0f6b78a31a73315e156
Mark Donnelly <mark@painless-security.com>
2023-03-31 15:25:21 -0400

    Update release number

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.6'
+VERSION = '0.0.7'
 DESCRIPTION = 'OpenLDAP OpenCensus Statistics'
 LONG_DESCRIPTION = 'A package to gather statistics for OpenLDAP and publish via OpenCensus.'
 
