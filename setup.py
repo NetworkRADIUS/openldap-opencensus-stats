1e4ecd5916ed039564ca82920809a9cd3a2c4f21
Nick Porter <nick@portercomputing.co.uk>
2023-11-20 19:44:45 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.9'
+VERSION = '0.0.10'
 DESCRIPTION = 'OpenLDAP OpenCensus Statistics'
 LONG_DESCRIPTION = 'A package to gather statistics for OpenLDAP and publish via OpenCensus.'
 
